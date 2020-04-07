345f6c5f68625f84bfafd5c98d1bff642e50f172
N.S <40781417+Crizyntch@users.noreply.github.com>
2020-04-07 09:14:29 +0700

    added new query filter

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import codecs
 from contextlib import closing
 
 #CONSTANTS
-API_URL = 'http://taist-2020-heroku.herokuapp.com/api/sensor_data?device_id=1&limit=1'
+API_URL = 'http://taist-2020-heroku.herokuapp.com/api/sensor_data?device_id=1&limit=1&type=predicted'
 
 app = Flask(__name__, template_folder = 'frontend_interface')
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -19,11 +19,11 @@ def index():
 @app.route('/event')
 def event_data():
     def post_data():
-        API_URL = 'http://taist-2020-heroku.herokuapp.com/api/sensor_data?device_id=1&limit=1'
+        API_URL = 'http://taist-2020-heroku.herokuapp.com/api/sensor_data?device_id=1&limit=1&type=predicted'
         fieldnames = ["device_id","roll","pitch","yaw","acc_x","acc_y","acc_z","label","type","timestamp"]
         json_dict = list()
         for num in range(1,10):
-            API_URL = f"http://taist-2020-heroku.herokuapp.com/api/sensor_data?device_id={num}&limit=1"
+            API_URL = f"http://taist-2020-heroku.herokuapp.com/api/sensor_data?device_id={num}&limit=1&type=predicted"
             with closing(requests.get(API_URL, stream=True)) as r:
                 text = r.text[1:-4]
                 if not text : continue
